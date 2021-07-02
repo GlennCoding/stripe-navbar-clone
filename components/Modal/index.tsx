2a4f81a35616cd94625bb29e5a56976d73223024
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-07-02 12:42:39 +0200

    Add animation fade in and out to Modal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,5 +1,21 @@
+import { motion } from 'framer-motion';
+
+const modal = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.3 } },
+};
 const Modal: React.FC = ({ children }) => {
-  return <div className='shadow-lg p-10'>{children}</div>;
+  return (
+    <motion.div
+      className={`shadow-lg p-10`}
+      variants={modal}
+      initial='hidden'
+      animate='visible'
+      exit='hidden'
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default Modal;
