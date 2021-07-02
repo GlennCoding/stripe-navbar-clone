916cf9af649c0a11cf75b2d763a55bcf7638170e
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-07-02 19:44:35 +0200

    Create siteLinks constants

---
+++ constants/siteLinks.tsx
@@ -0,0 +1,83 @@
+import { MdWork } from 'react-icons/md';
+
+const iconStyles = 'w-5 h-5';
+
+const siteLinks = {
+  products: {
+    title: 'Products',
+    links: [
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Payments',
+        description: 'Online Payments',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Issuing',
+        description: 'Card Creation',
+      },
+    ],
+  },
+  useCases: {
+    title: 'Use cases',
+    links: [
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Ecommerce',
+        description: 'Unify online and in-person payments',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'SaaS',
+        description: 'Manage recurring billing and subscriptions',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'SaaS',
+        description: 'Manage recurring billing and subscriptions',
+      },
+    ],
+  },
+  developers: {
+    title: 'Developers',
+    links: [
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Documentation',
+        description: 'Start integrating Stripe’s products and tools',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Full API reference ',
+        description: '',
+      },
+    ],
+  },
+  company: {
+    title: 'Company',
+    links: [
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'About Stripe',
+        description: '',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Jobs',
+        description: '',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Customers',
+        description: '',
+      },
+      {
+        icon: <MdWork className={iconStyles} />,
+        title: 'Newsroom',
+        description: '',
+      },
+    ],
+  },
+};
+
+export default siteLinks;
