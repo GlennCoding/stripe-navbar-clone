9ff2f705a7921852a2a19dcf2211f170136f3a88
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-07-04 11:12:15 +0200

    Extract SiteLinks component from Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,48 +1,18 @@
-import { useState } from 'react';
 import { FaStripe } from 'react-icons/fa';
 import { MdMenu } from 'react-icons/md';
-import { motion, AnimateSharedLayout } from 'framer-motion';
 import Button from '../Button';
-import ModalList from '../ModalList';
-import siteLinks from '../../constants/siteLinks';
+import SiteLinks from '../SiteLinks';
 
 const Navbar: React.FC = () => {
-  const [activeMenu, setActiveMenu] = useState<string>('');
-  console.log(activeMenu);
   return (
     <>
       <header className='flex justify-center'>
         <div className='flex justify-between items-center px-4 mx-4 py-3 w-full max-w-4xl'>
-          <FaStripe
-            size='48'
-            className='fill-current text-white cursor-pointer'
-          />
+          <div className='fill-current text-white cursor-pointer'>
+            <FaStripe size='48' />
+          </div>
           <div className='hidden md:flex'>
-            <AnimateSharedLayout>
-              {Object.entries(siteLinks).map((item) => {
-                const title = item[1].title;
-                return (
-                  <div key={title} className='relative'>
-                    <button
-                      className='px-5 py-4 cursor-pointer text-white font-medium'
-                      onMouseEnter={() => setActiveMenu(title)}
-                      onMouseLeave={() => setActiveMenu('')}
-                    >
-                      {title}
-                    </button>
-                    {title === activeMenu && (
-                      <motion.div
-                        layoutId='hover'
-                        className='rounded-t-full absolute bg-white w-3 h-3 inset-x-2/4 top-12'
-                      />
-                    )}
-                  </div>
-                );
-              })}
-            </AnimateSharedLayout>
-            <button className='px-5 py-3 cursor-pointer text-white font-medium'>
-              <a href=''>Pricing</a>
-            </button>
+            <SiteLinks />
           </div>
           <div>
             <div className='hidden md:block'>
@@ -56,11 +26,6 @@ const Navbar: React.FC = () => {
               </Button>
             </div>
           </div>
-          <ModalList
-            activeMenu={activeMenu}
-            setActiveMenu={setActiveMenu}
-            siteLinks={siteLinks}
-          />
         </div>
       </header>
     </>
---
+++ components/SiteLinks/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { motion, AnimateSharedLayout } from 'framer-motion';
+import ModalList from '../ModalList';
+import siteLinks from '../../constants/siteLinks';
+
+const SiteLinks: React.FC = () => {
+  const [activeMenu, setActiveMenu] = useState<string>('');
+  return (
+    <>
+      <AnimateSharedLayout>
+        {Object.entries(siteLinks).map((item) => {
+          const title = item[1].title;
+          return (
+            <div key={title} className='relative'>
+              <button
+                className='px-5 py-4 cursor-pointer text-white font-medium'
+                onMouseEnter={() => setActiveMenu(title)}
+                onMouseLeave={() => setActiveMenu('')}
+              >
+                {title}
+              </button>
+              {title === activeMenu && (
+                <motion.div
+                  layoutId='hover'
+                  className='rounded-t-full absolute bg-white w-3 h-3 inset-x-2/4 top-12'
+                />
+              )}
+            </div>
+          );
+        })}
+      </AnimateSharedLayout>
+      <button className='px-5 py-3 cursor-pointer text-white font-medium'>
+        <a href=''>Pricing</a>
+      </button>
+      <ModalList
+        activeMenu={activeMenu}
+        setActiveMenu={setActiveMenu}
+        siteLinks={siteLinks}
+      />
+    </>
+  );
+};
+
+export default SiteLinks;
